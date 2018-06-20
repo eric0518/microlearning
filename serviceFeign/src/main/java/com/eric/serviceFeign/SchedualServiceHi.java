fa76c8523d58739de2be172e20c830075a4f0a89
xiaogangwang <xiaogangwang@yeah.net>
2018-06-20 10:43:26 +0800

    Added service Feign

---
+++ serviceFeign/src/main/java/com/eric/serviceFeign/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.eric.serviceFeign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by xiaog on 2018/6/20.
+ */
+
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value="/hi", method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value="name") String name);
+}
