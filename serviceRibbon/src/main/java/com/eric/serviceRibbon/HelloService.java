cda8aead790b7eace59679be13e1fc458fcacd94
xiaogangwang <xiaogangwang@yeah.net>
2018-06-20 14:40:21 +0800

    Addede hytrix to ribbon

--- serviceRibbon/src/main/java/com/eric/serviceRibbon/HelloService.java
+++ serviceRibbon/src/main/java/com/eric/serviceRibbon/HelloService.java
@@ -1,5 +1,7 @@
 package com.eric.serviceRibbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,9 +15,12 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod ="hiError")
     public String hiService(String name) {
-        //return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class).toString();http://localhost:8763/hi?name=forezp
         return restTemplate.getForObject("http://service-hi/hi?name=" + name, String.class).toString();
+    }
 
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
--- serviceRibbon/src/main/java/com/eric/serviceRibbon/ServiceRibbonApplication.java
+++ serviceRibbon/src/main/java/com/eric/serviceRibbon/ServiceRibbonApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@EnableHystrix
 public class ServiceRibbonApplication {
 
 	public static void main(String[] args) {
